14109f01c349cd7acefe3d992b8762a44e580a09
Guilherme Tinen Ortolani <guilherme.tinen@cantacom.com.br>
2022-03-21 17:44:58 -0300

    seo of nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,27 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'desafio-frontend-ow-interactive',
+    title: 'Mkt Place',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-br'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+      {hid: 'keywords',name: 'keywords',content: `Comunicação, Cantaloop, Cantacom`},
+      {hid: 'author',name: 'author',content: `Cantacom`},
+      {hid: 'description',name: 'description',content: `Acreditamos que criatividade não depende de tamanho nem plataforma. Marcas precisam entregar conteúdo, experiências e informações em diversos canais, da maneira mais assertiva possível. É para isso que estamos aqui.`},
+
+      {hid: 'og:locale',name: 'og:locale',content: `pt_BR`},
+      {hid: 'og:site_name',name: 'og:site_name',content: `Cantacom`},
+      {hid: 'og:title',name: 'og:title',content: `Cantacom - Cantaloop Comunicação mas pode chamar de Cantacom`},
+      {hid: 'og:type',name: 'og:type',content: `website`},
+      {hid: 'og:image',name: 'og:image',content: `http://www.cantacom.com.br/website/_img/img-social.png`},
+      {hid: 'og:url',name: 'og:url',content: `www.cantacom.com.br/`},
+      {hid: 'og:description',name: 'og:description',content: `Acreditamos que criatividade não depende de tamanho nem plataforma. Marcas precisam entregar conteúdo, experiências e informações em diversos canais, da maneira mais assertiva possível. É para isso que estamos aqui.`},
+      {hid: 'og:image:width',name: 'og:image:width',content: `800`},
+      {hid: 'og:image:height',name: 'og:image:height',content: `600`},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
